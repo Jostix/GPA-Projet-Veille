d32149d5ab21a570affa244792b4f892dc4c7dc7
romain.ducret1 <romain.ducret1@IG13-DUCRETR.intra.eiaj.ch>
2015-12-01 11:16:47 +0100

    Ajout de servlets pour l'affihage du fil, modification des fichiers de configurations en conséquence

---
+++ src/java/ch/hearc/ig/gpa/servlet/ServletFil.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.gpa.servlet;
+
+import ch.hearc.ig.gpa.business.Message;
+import ch.hearc.ig.gpa.exceptions.ConnectionProblemException;
+import ch.hearc.ig.gpa.services.MessageService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Romain Ducret <romain.ducret1@he-arc.ch>
+ */
+public class ServletFil extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        //request.getRequestDispatcher("includes/header.jsp").include(request, response);
+        // request.getRequestDispatcher("includes/navbar.jsp").include(request, response);
+
+        try {
+            HtmlHttpUtils.doHeader("<span class='glyphicon glyphicon-th-list'></span> Fil d'actualité", out);
+
+            Set<Message> searchResultsList = MessageService.findAllMessage();
+            out.println("Messages trouvées:");
+            out.println(searchResultsList.size());
+
+            HtmlHttpUtils.doFooter(out);
+
+        } catch (ConnectionProblemException e) {
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
