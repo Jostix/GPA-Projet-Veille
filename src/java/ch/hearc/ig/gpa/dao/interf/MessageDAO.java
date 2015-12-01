e1f4d471814e3ceb351a50063c67ab1750f845f7
Thierry Hubmann <thierry.hubmann@he-arc.ch>
2015-12-01 12:00:03 +0100

    Implémentation de la méthode retournant les top messages en s'appuiyant sur le nombre de retweet

--- src/java/ch/hearc/ig/gpa/dao/interf/MessageDAO.java
+++ src/java/ch/hearc/ig/gpa/dao/interf/MessageDAO.java
@@ -7,6 +7,7 @@ package ch.hearc.ig.gpa.dao.interf;
 
 import ch.hearc.ig.gpa.business.Message;
 import ch.hearc.ig.gpa.exceptions.ConnectionProblemException;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -27,5 +28,5 @@ public interface MessageDAO {
 
     public Set<Message> getAllMessage() throws ConnectionProblemException;
 
-    public Set<Message> getTop5Message() throws ConnectionProblemException;
+    public List<Message> getTop5Message() throws ConnectionProblemException;
 }
